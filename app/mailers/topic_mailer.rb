8618baecb7f27b343b3d5194d3bb48a58bb93331
Paul Miller <paul@paulmillr.com>
2012-08-27 09:11:39 +0300

    Add email notifications for users posted in threads.

---
+++ app/mailers/topic_mailer.rb
@@ -0,0 +1,22 @@
+class TopicMailer < ActionMailer::Base
+  default from: 'noreply@ost.io'
+
+  def new_post_email(post)
+    @post = post
+    topic = post.topic
+    repo = topic.repo
+    post_user = post.user
+    repo_user = repo.user
+    @url = "ost.io/#{repo_user.login}/#{repo.name}/topics/#{topic.number}"
+
+    user_email = post_user.email
+    emails = topic.poster_emails.select { |email| email != user_email }
+    subject = "Re: [#{repo.name}] #{post.text}"
+
+    emails.each do |email|
+      mail(to: email, subject: subject)
+    end
+  end
+
+  handle_asynchronously :new_post_email
+end
