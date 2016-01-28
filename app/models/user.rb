6fa5775248f04241274f2d07fed052e774494fe5
Gosha Arinich <me@goshakkk.name>
2016-01-28 20:32:58 +0300

    Make updates to settings actually work

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   before_save :ensure_authentication_token
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :github_token, :enabled_email_notifications
+  attr_accessible :enabled_email_notifications
 
   has_many :repos, dependent: :destroy
   has_many :topics, dependent: :destroy
